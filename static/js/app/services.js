36d4fec53cb2bb731a71c212d4651079a1093dfb
Polina Leonova <polina.ostapenko.88@gmail.com>
2016-12-03 14:36:01 +0200

    add controller for two players, add servise 'share', add directives 'frame', 'dialog window', change in logic of controller for one player

--- static/js/app/services.js
+++ static/js/app/services.js
@@ -1,4 +1,29 @@
 'use strict';
 
-/* Services */
 
+var finderServices = angular.module('finderServices', []);
+finderServices.factory('share', function(){
+    var data = {};
+    data.winner = '';
+    data.score = 0;
+    data.setScores = function(scores) {
+        data.score = scores;
+    };
+    data.subtractScore = function() {
+        data.score = data.score - 1;
+        return data.score
+    };
+    return data
+
+});
+finderServices.service('$dialog',['$compile','$timeout','$rootScope', function($compile,$timeout, $rootScope){
+    return function(content){
+        var $dialogw = angular.element('<div dialog-window="'+content+'"></div>');
+        angular.element(document.body).append($dialogw);
+
+        $timeout(function() {
+             var newScope = $rootScope.$new(true);
+             $compile($dialogw)(newScope);
+         });
+    }
+}]);
